f4e0b35ab6673457a5c9e95c1a6187881e90a2f0
Santiago Zampese <santiagozampese@gmail.com>
2026-07-02 22:56:14 -0300

    Receive Correction

--- Program.cs
+++ Program.cs
@@ -2,153 +2,181 @@
 using System.Text;
 using Newtonsoft.Json;
 using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Components.Endpoints;
 
-var builder = WebApplication.CreateBuilder(args);
-string port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-
-var app = builder.Build();
-app.UseWebSockets();
-
-var Rooms = new ConcurrentDictionary<string , Room>();
-
-Rooms.TryAdd("Default", new Room("Default", null, 30));
-
-app.Map("/ws", async (HttpContext context) =>
+public static class Program
 {
-    if (context.WebSockets.IsWebSocketRequest)
+    public static WebApplication? app = null;
+    public static ConcurrentDictionary<string, Room> Rooms = new();
+
+    public static string? room = null;
+    public static void Main(string[] args)
+    {  
+        var builder = WebApplication.CreateBuilder(args);
+        string port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+        builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+        app = builder.Build();
+        app.UseWebSockets();
+
+        Rooms.TryAdd("Default", new Room("Default", null, 30));
+
+        Config();
+
+        app.Run();
+    }
+
+    public static void Config()
     {
-        byte[] buffer = new byte[1024 * 4];
-
-        var room = context.Request.Query["room"].ToString();
-
-        using WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
-
-        (bool completed, WebSocketReceiveResult? result) = await ReceiveRequests(webSocket, Rooms);
-
-        if (!completed)
-        {         
-            string json = JsonConvert.SerializeObject(Rooms);
-            var send = Encoding.UTF8.GetBytes(json);
-
-            await webSocket.SendAsync(new ArraySegment<byte>(send), WebSocketMessageType.Text, true, CancellationToken.None);
-
-            if (string.IsNullOrEmpty(room))
+        
+        app!.Map("/ws", async (HttpContext context) =>
+        {
+            if (context.WebSockets.IsWebSocketRequest)
             {
+                room = context.Request.Query["room"].ToString();
+
+                using WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+
+                if (!string.IsNullOrEmpty(room)) Console.WriteLine($"Client connected in room: {room}");
+
+                await ClientTratament(webSocket);
+            }
+            else
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        });
+    }
+
+    static async Task ClientTratament(WebSocket socket)
+    {
+        try
+        {
+            byte[] buffer = new byte[1024 * 4];
+            while (socket.State == WebSocketState.Open)
+            {
+                bool canSend = true;
+
+                var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                
+                    byte[] sendBuffer = Encoding.UTF8.GetBytes(message);
+
+                    Sendable? sendable = JsonConvert.DeserializeObject<Sendable>(message);
+
+                    List<WebSocket> roomClients = new();
+
+                    if (sendable != null)
+                    {
+                        if (sendable.type == "request")
+                        {
+                            bool completed = await ReceiveRequests(socket, result, Rooms);
+                            canSend = false;
+                        }
+                    }
+
+                    if (canSend)
+                    {              
+                        lock (Rooms)
+                        {
+                            if (Rooms.ContainsKey(room!))
+                            {               
+                                roomClients = Rooms[room!].clients.ToList();
+                                break;
+                            }
+                        }
+
+                        foreach (var client in roomClients)
+                        {
+                            if (client.State == WebSocketState.Open && client != socket)
+                            {
+                                await client.SendAsync(new ArraySegment<byte>(sendBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                            }
+                        }
+                    }
+                }
+                else if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Disconnected", CancellationToken.None);
+                }
+            }
+        }
+        catch {}
+        finally
+        {
+            lock (Rooms)
+            {
+                if (Rooms.ContainsKey(room!))
+                {               
+                    Rooms[room!].clients.Remove(socket);
+                }
+            }
+
+            Console.WriteLine($"User left room {room}!");
+        }
+    }
+
+    static async Task CreateRoom(WebSocket socket, Room room, ConcurrentDictionary<string, Room> rooms)
+    {
+        try
+        {          
+            if (room != null)
+            {
+                rooms.TryAdd(room?.name!, room!);
+                rooms[room?.name!].clients.Add(socket);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+    }
+
+    static async Task<bool> ReceiveRequests(WebSocket socket, WebSocketReceiveResult receive, ConcurrentDictionary<string, Room> rooms)
+    {
+        WebSocketReceiveResult? result = null;
+        try
+        {
+            byte[] buffer = new byte[1024 * 4];
+            result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            string json = Encoding.UTF8.GetString(buffer, 0, result.Count);
+            Sendable? sendable = JsonConvert.DeserializeObject<Sendable>(json);
+
+            if (sendable != null && sendable.message!.message == "createRoom")
+            {
+                await CreateRoom(socket, sendable?.room!, rooms);
+            }
+
+            else if (sendable?.message!.message == "connect")
+            {
+                if (sendable?.room!.name != null) room = sendable.room.name;
+            }
+
+            else if (sendable?.message!.message == "getRooms")
+            {
+                json = JsonConvert.SerializeObject(Rooms);
+                var send = Encoding.UTF8.GetBytes(json);
+
+                await socket.SendAsync(new ArraySegment<byte>(send), WebSocketMessageType.Text, true, CancellationToken.None);
+
                 if (result != null)
                 {           
                     json = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                    Sendable? sendable = JsonConvert.DeserializeObject<Sendable>(json);
+                    Sendable? sendableObj = JsonConvert.DeserializeObject<Sendable>(json);
 
                     if (sendable != null && sendable.room != null && sendable.room.name != null) room = sendable.room.name;
                 }
             }
-            
-            Rooms[room].clients.Add(webSocket);
+            return true;
         }
-
-        Console.WriteLine($"Client conncted in room: {room}");
-
-        await ClientTratament(webSocket, room, Rooms);
-    }
-    else
-    {
-        context.Response.StatusCode = StatusCodes.Status400BadRequest;
-    }
-});
-
-app.Run();
-
-static async Task ClientTratament(WebSocket socket, string roomName, ConcurrentDictionary<string, Room> rooms)
-{
-    try
-    {
-        byte[] buffer = new byte[1024 * 4];
-        while (socket.State == WebSocketState.Open)
+        catch (Exception e)
         {
-            var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-    
-            if (result.MessageType == WebSocketMessageType.Text)
-            {
-                string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-            
-                byte[] sendBuffer = Encoding.UTF8.GetBytes(message);
-
-                List<WebSocket> roomClients = new();;
-                lock (rooms)
-                {
-                    if (rooms.ContainsKey(roomName))
-                    {               
-                        roomClients = rooms[roomName].clients.ToList();
-                        break;
-                    }
-                }
-
-                foreach (var client in roomClients)
-                {
-                    if (client.State == WebSocketState.Open && client != socket)
-                    {
-                        await client.SendAsync(new ArraySegment<byte>(sendBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
-                    }
-                }
-            }
-            else if (result.MessageType == WebSocketMessageType.Close)
-            {
-                await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Disconnected", CancellationToken.None);
-            }
+            Console.WriteLine($"Error to receive: {e.Message}");
+            return false;
         }
-    }
-    catch {}
-    finally
-    {
-        lock (rooms)
-        {
-            if (rooms.ContainsKey(roomName))
-            {               
-                rooms[roomName].clients.Remove(socket);
-            }
-        }
-
-        Console.WriteLine($"User left room {roomName}!");
-    }
-}
-
-static async Task CreateRoom(WebSocket socket, Room room, ConcurrentDictionary<string, Room> rooms)
-{
-    try
-    {          
-        if (room != null)
-        {
-            rooms.TryAdd(room?.name!, room!);
-            rooms[room?.name!].clients.Add(socket);
-        }
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine($"Error: {e.Message}");
-    }
-}
-
-static async Task<(bool, WebSocketReceiveResult?)> ReceiveRequests(WebSocket socket, ConcurrentDictionary<string, Room> rooms)
-{
-    WebSocketReceiveResult? result = null;
-    try
-    {
-        byte[] buffer = new byte[1024 * 4];
-        result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-
-        string json = Encoding.UTF8.GetString(buffer, 0, result.Count);
-        Sendable? sendable = JsonConvert.DeserializeObject<Sendable>(json);
-
-        if (sendable != null && sendable.message!.message == "createRoom")
-        {
-            await CreateRoom(socket, sendable?.room!, rooms);
-        }
-        return (true, null);
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine($"Error to receive: {e.Message}");
-        return (false, result);
     }
 }
--- obj/Debug/net10.0/ChatServer.AssemblyInfo.cs
+++ obj/Debug/net10.0/ChatServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62f53cbac6c4969e84129af9b0886a095f39ca30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+323d217fd2d8e4df2a63cf11d6e8d6fc4d330adb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
